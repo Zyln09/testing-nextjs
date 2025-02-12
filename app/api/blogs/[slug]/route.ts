d7ee88a66193d897410d2a40a760d305303045c7
ZylnGovender <zylng@runninghill.co.za>
2025-02-12 20:30:41 +0200

    logged error

--- app/api/blogs/[slug]/route.ts
+++ app/api/blogs/[slug]/route.ts
@@ -14,6 +14,7 @@ export async function GET(req: Request, { params }: { params: { slug: string } }
 
         return NextResponse.json(blog[0]);
     } catch (error) {
+        console.log(error);
         return NextResponse.json({ error: "Failed to fetch blog" }, { status: 500 });
     }
 }
